afc08b7274d7e718c4e378e50dbac98e61c765e3
tesdeve <tesdeve@gmail.com>
2020-06-25 12:17:37 +0100

    Prepares everything to start settings for sending invitations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_093048) do
+ActiveRecord::Schema.define(version: 2020_06_24_080208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 2020_05_03_093048) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "lastname"
+    t.string "username"
+    t.string "telephone"
+    t.boolean "terms", default: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -70,12 +75,6 @@ ActiveRecord::Schema.define(version: 2020_05_03_093048) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.string "lastname"
-    t.string "username"
-    t.string "telephone"
-    t.boolean "terms", default: false
-    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
